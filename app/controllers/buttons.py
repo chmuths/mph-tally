e2541700d40362dc538237be7bb0d73b5a3fd7c6
cmuths <christian.muths@al-enterprise.com>
2021-05-12 20:19:51 +0200

    Scanning keyboard by software loop. Works much better.

--- app/controllers/buttons.py
+++ app/controllers/buttons.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+from threading import Timer
 import time
 # noinspection PyUnresolvedReferences
 import RPi.GPIO as GPIO
@@ -13,57 +14,66 @@ class Buttons:
 
     def __init__(self, config, companion, tally_blinker, logger):
         # Define GPIO ports and other properties for buttons
+        GPIO.setup(config['port'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        self.logger = logger
         self.configure_button(config)
         self.companion = companion
-        self.state = 1
+        self.debounce_counter = 0
+        self.port = config['port']
+        self.reverse = config.get('reverse', 0)
+        self.state = 0 if self.reverse else 1
+        self.state_str = "up"
         self.tally = tally_blinker
         self.visual_echo = False
-        self.logger = logger
 
-        GPIO.setup(config['port'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        if config.get('mode', 'Normal') == 'PTT':
-            GPIO.add_event_detect(self.port, GPIO.BOTH, callback=self.button_action, bouncetime=40)
-        else:
-            GPIO.add_event_detect(self.port, GPIO.FALLING, callback=self.button_pressed, bouncetime=150)
+        self.run = True
+        self.trigger_timer()
+
+    def trigger_timer(self):
+        if self.run:
+            t = Timer(0.01, self.debounce)
+            t.start()
+
+    def debounce(self):
+        gpio_value = GPIO.input(self.port)
+        if self.state != gpio_value:
+            self.debounce_counter += 1
+            if self.debounce_counter > 3:
+                self.state = gpio_value
+                self.debounce_counter = 0
+                self.button_action()
+        self.trigger_timer()
 
     def configure_button(self, config):
         self.config = config
         self.name = config['name']
-        self.port = config['port']
-        self.reverse = config.get('reverse', 0)
         self.bank = config.get('bank')
         self.number = config.get('number')
 
-    def button_action(self, port):
-        gpio_value = GPIO.input(port)
+    def button_action(self):
         # self.logger.info(f"Reverse {self.reverse} on port {port} with value {gpio_value}")
-        if gpio_value != self.reverse:
-            self.logger.info(f"Button {self.config['name']} on port {port} was released")
-            self.state = 1
-            self.execute_action('up')
+        if self.state != self.reverse:
+            self.logger.info(f"Button {self.config['name']} on port {self.port} was released")
+            self.state_str = 'up'
+            self.execute_action()
         else:
-            self.logger.info(f"Button {self.config['name']} on port {port} was pressed")
-            self.state = 0
-            self.execute_action('down')
-
-
-    def button_pressed(self, port):
-        self.logger.info(f"Button {self.config['name']} on port {port} was pressed")
-        self.execute_action(None)
+            self.logger.info(f"Button {self.config['name']} on port {self.port} was pressed")
+            self.state_str = 'down'
+            self.execute_action()
 
     def button_web_test(self):
         self.logger.info("Enter TEST TOGGLE")
-        if self.state == 1:
+        if self.state_str == "down":
             self.logger.info(f"Test Button {self.config['name']} was released")
-            self.state = 0
-            self.execute_action('up')
+            self.state_str = 'up'
+            self.execute_action()
         else:
             self.logger.info(f"Test Button {self.config['name']} was pressed")
-            self.state = 1
-            self.execute_action('down')
+            self.state_str = 'down'
+            self.execute_action()
 
-    def execute_action(self, action):
-        self.companion.press_key(self.config.get('bank'), self.config.get('number'), action)
+    def execute_action(self):
+        self.companion.press_key(self.config.get('bank'), self.config.get('number'), self.state_str)
 
         if self.visual_echo:
             if self.tally:
